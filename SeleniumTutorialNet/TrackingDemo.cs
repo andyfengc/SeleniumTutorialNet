786dedad4d55d54a93306ab9350dfc139b5d48a6
Andy Feng <andyfengc@gmail.com>
2015-11-05 22:59:28 -0500

    debug

--- SeleniumTutorialNet/TrackingDemo.cs
+++ SeleniumTutorialNet/TrackingDemo.cs
@@ -20,6 +20,8 @@ namespace SeleniumTutorialNet
         {
             // initialize a WebDriver instance
             IWebDriver driver = new PhantomJSDriver();
+            WebDriverWait driverWait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            driver.Manage().Window.Maximize();
             // load search page
             driver.Navigate().GoToUrl("http://www.purolator.com/purolator/ship-track/tracking-summary.page?submit=true&componentID=1359477580240");
             // wait 5 seconds
@@ -34,32 +36,35 @@ namespace SeleniumTutorialNet
                 }
             }
 
+
             Console.WriteLine("Page title: " + driver.Title);
             // enter tracking number and submit
-            IWebElement element = driver.FindElement(By.CssSelector("textarea#search"));
+            IWebElement element = driver.FindElement(By.CssSelector("form textarea#search"));
             element.SendKeys("JFV242486848");
             element.Submit();
-            // wait 5 seconds
-            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(5));
-            // write result page
-            using (Stream stream = File.Open(@"d:\delete\selenium\2.html", FileMode.Create, FileAccess.ReadWrite))
-            {
-                using (BinaryWriter writer = new BinaryWriter(stream))
-                {
-                    byte[] bytes = Encoding.UTF8.GetBytes(driver.PageSource);
-                    stream.Write(bytes, 0, bytes.Length);
-                }
-            }
+            //// wait 5 seconds
+            //driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(5));
+            //// write result page
+            //using (Stream stream = File.Open(@"d:\delete\selenium\2.html", FileMode.Create, FileAccess.ReadWrite))
+            //{
+            //    using (BinaryWriter writer = new BinaryWriter(stream))
+            //    {
+            //        byte[] bytes = Encoding.UTF8.GetBytes(driver.PageSource);
+            //        stream.Write(bytes, 0, bytes.Length);
+            //    }
+            //}
 
 
 
 
-            // locate the tracking history table
+                // locate the tracking history table
             var trackingHistory = driver.FindElement(By.CssSelector("#historyDiv table#history tbody"));
             string result = trackingHistory.Text;
             var rows = trackingHistory.FindElements(By.TagName("tr")).ToList();
             string locationPath = "//td[@class='location']";
-            var locations = driver.FindElements(By.XPath(locationPath));
+            //var locations = driverWait.Until(drv => drv.FindElements(By.XPath("//td[@class='location']")));
+            //driverWait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//td[@class='location']")));
+             var locations = driver.FindElements(By.XPath(locationPath));
             foreach (var location in locations)
             {
                 string a = location.Text;
